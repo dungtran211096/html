be5173b99d2a352fc2c20a230beb91c4de440dc1
DungTran211096 <dungtran2196@gmail.com>
2016-12-22 17:02:59 +0700

    user data migrate update

--- app/User.php
+++ app/User.php
@@ -23,7 +23,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-    public function user_datas(){
-        return $this->hasOne('App\UserData', foreign_key);
+    public function data(){
+        return $this->hasOne(App/UserData::class);
     }
 }
